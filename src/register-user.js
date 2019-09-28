1d0f48cfb3926403a453221c6dcd348de9cf7bf5
York Huang <york.huang@honeywell.com>
2019-09-28 11:53:51 +1000

    curried everything

--- src/register-user.js
+++ src/register-user.js
@@ -5,8 +5,8 @@ const equals = x => y => x === y;
 const isNotEmpty = x => x.length > 0;
 const matches = regex => x => regex.test(x);
 const prop = key => o => o[key];
-const propEquals = (key, value) => o => o[key] === value;
-const validate = (predicate, errorMessage) => x =>
+const propEquals = key => value => o => o[key] === value;
+const validate = predicate => errorMessage => x =>
   Promise.resolve(predicate(x)).then(valid =>
     valid ? Promise.resolve(x) : Promise.reject(errorMessage)
   );
@@ -21,22 +21,22 @@ const errorResult = reason => ({
 
 const validateUserName = isAvailable => userName =>
   Promise.resolve(userName)
-    .then(validate(isNotEmpty, messages.userName.empty))
-    .then(validate(matches(userNameRegex), messages.userName.incorrectFormat))
-    .then(validate(isAvailable, messages.userName.notAvailable))
+    .then(validate(isNotEmpty)(messages.userName.empty))
+    .then(validate(matches(userNameRegex))(messages.userName.incorrectFormat))
+    .then(validate(isAvailable)(messages.userName.notAvailable))
     .then(okResult, errorResult);
 
 const validatePassword = confirmPassword => password =>
   Promise.resolve(password)
-    .then(validate(isNotEmpty, messages.password.empty))
-    .then(validate(matches(passwordRegex), messages.password.incorrectFormat))
-    .then(validate(equals(confirmPassword), messages.password.confirm))
+    .then(validate(isNotEmpty)(messages.password.empty))
+    .then(validate(matches(passwordRegex))(messages.password.incorrectFormat))
+    .then(validate(equals(confirmPassword))(messages.password.confirm))
     .then(okResult, errorResult);
 
 const checkResults = results =>
   results.every(prop('ok'))
     ? Promise.resolve(results.map(prop('value')))
-    : Promise.reject(results.filter(propEquals('ok', false)).map(prop('reason')));
+    : Promise.reject(results.filter(propEquals('ok')(false)).map(prop('reason')));
 
 module.exports = isUserNameAvailable => (userName, password, confirmPassword) =>
   Promise.all([
